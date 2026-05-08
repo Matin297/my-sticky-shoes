af0fbd45a4eab5dc0c922dfeff894b318f97bc70
Matin Mir <asmamir297@gmail.com>
2026-05-08 15:27:22 +0330

    feat: added a type-safe global link button component

---
+++ components/LinkButton.tsx
@@ -0,0 +1,15 @@
+import { Button, type ButtonProps } from "@mui/material";
+import type { Route } from "next";
+import Link from "next/link";
+
+interface NavButtonProps extends Omit<ButtonProps, "href"> {
+  href: Route;
+}
+
+export function LinkButton({ href, children, ...props }: NavButtonProps) {
+  return (
+    <Button LinkComponent={Link} href={href} {...props}>
+      {children}
+    </Button>
+  );
+}
