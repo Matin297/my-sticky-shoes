e795826e80301fda3b2f209db8153d69ef15c0b0
Matin Mir <asmamir297@gmail.com>
2026-05-10 17:34:00 +0330

    chore(proxy): added route authorization

---
+++ proxy.ts
@@ -0,0 +1,35 @@
+import { cookies } from "next/headers";
+import { type NextRequest, NextResponse } from "next/server";
+import { ACCESS_TOKEN } from "@/lib/cookies";
+
+const PUBLIC_ROUTES = ["/login", "/signup", "/sw.js", "/manifest.webmanifest"];
+
+export async function proxy(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  const isPublicRoute = PUBLIC_ROUTES.includes(pathname);
+  const isPrivateRoute = !isPublicRoute;
+  const accessToken = (await cookies()).get(ACCESS_TOKEN)?.value;
+
+  if (isPublicRoute) {
+    if (accessToken) {
+      return NextResponse.redirect(new URL("/", request.nextUrl));
+    }
+
+    return NextResponse.next();
+  }
+
+  if (isPrivateRoute && !accessToken) {
+    const redirectUrl = new URL("/login", request.url);
+    redirectUrl.searchParams.set("callbackUrl", pathname);
+    return NextResponse.redirect(redirectUrl);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    "/((?!api|_next|public|.*\\.(?:ico|png|jpg|jpeg|gif|webp|avif|svg|css|js|map|txt|xml)$).*)",
+  ],
+};
