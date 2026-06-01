98bb50b5dd483133adac456ca3558e28e6c6e9de
Matin Mir <asmamir297@gmail.com>
2026-06-01 14:27:46 +0330

    refactor: added active state to header nav links

--- app/(main)/_components/Header.tsx
+++ app/(main)/_components/Header.tsx
@@ -1,16 +1,64 @@
 "use client";
 
+import { Home, Person, ShoppingBag } from "@mui/icons-material";
 import { AppBar, Box, Toolbar } from "@mui/material";
+import type { Route } from "next";
+import { usePathname } from "next/navigation";
+import type { ReactNode } from "react";
 import { LinkButton } from "@/components/LinkButton";
 
+interface NavLink {
+  id: number;
+  title: string;
+  path: Route;
+  icon: ReactNode;
+}
+
+const NAV_LINKS: NavLink[] = [
+  {
+    id: 1,
+    title: "home",
+    path: "/",
+    icon: <Home />,
+  },
+  {
+    id: 2,
+    title: "products",
+    path: "/products",
+    icon: <ShoppingBag />,
+  },
+  {
+    id: 3,
+    title: "profile",
+    path: "/profile",
+    icon: <Person />,
+  },
+];
+
 export default function Header() {
+  const pathname = usePathname();
+
+  const isNavLinkActive = (navLinkPath: string) => {
+    if (navLinkPath === "/") return pathname === "/";
+    return pathname.startsWith(navLinkPath);
+  };
+
   return (
     <AppBar position="static" color="transparent" elevation={0}>
       <Toolbar disableGutters sx={{ justifyContent: "space-between" }}>
         <Box sx={{ display: "flex", alignItems: "center" }}>
-          <LinkButton href="/profile">Profile</LinkButton>
-          <LinkButton href="/profile">Profile</LinkButton>
-          <LinkButton href="/profile">Profile</LinkButton>
+          {NAV_LINKS.map(({ id, path, title, icon }) => {
+            return (
+              <LinkButton
+                color={isNavLinkActive(path) ? "secondary" : "primary"}
+                key={id}
+                href={path}
+                startIcon={icon}
+              >
+                {title}
+              </LinkButton>
+            );
+          })}
         </Box>
       </Toolbar>
     </AppBar>
